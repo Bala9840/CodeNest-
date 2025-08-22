c4a3edba97ffc878222a2a9f8c8a9cc697011876
Bala9840 <barathbalaji9840@gmail.com>
2025-08-23 00:24:54 +0530

    after error fix

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Editor.css';
+
+const Editor = ({ language, value, onChange, height = '50vh' }) => {
+  const handleChange = (e) => {
+    onChange(e.target.value);
+  };
+
+  return (
+    <div className="editor-container rounded-lg overflow-hidden border border-gray-700">
+      <textarea
+        className="code-editor"
+        value={value}
+        onChange={handleChange}
+        style={{ height }}
+        placeholder="Write your code here..."
+      />
+    </div>
+  );
+};
+
+
+export default Editor;
